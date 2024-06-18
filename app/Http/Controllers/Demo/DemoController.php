e7ef1417187978b38e3eea30203c16f866512d70
satria <satria.perdana@gli.id>
2024-06-18 10:12:19 +0700

    add dashboard admin menggunakan cube + supplier

--- app/Http/Controllers/Demo/DemoController.php
+++ app/Http/Controllers/Demo/DemoController.php
@@ -7,5 +7,17 @@ use Illuminate\Http\Request;
 
 class DemoController extends Controller
 {
-    //
+
+    public function HomeMain(){
+        return view('frontend.index');
+    }// end mehtod 
+    
+    public function Index(){
+        return view('about');
+    } // end mehtod 
+
+
+    public function ContactMethod(){
+        return view('contact');
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,28 +1,55 @@
 <?php
 
-use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Demo\DemoController;
+use App\Http\Controllers\AdminController;
+use App\Http\Controllers\Pos\SupplierController;
+
 
 Route::get('/', function () {
     return view('welcome');
 });
+ 
 
-Route::get('/about', function () {
-    return view('about');
+Route::controller(DemoController::class)->group(function () {
+    Route::get('/about', 'Index')->name('about.page')->middleware('check');
+    Route::get('/contact', 'ContactMethod')->name('cotact.page');
 });
 
-Route::get('/contact', function () {
-    return view('contact');
+
+ // Admin All Route 
+Route::controller(AdminController::class)->group(function () {
+    Route::get('/admin/logout', 'destroy')->name('admin.logout');
+    Route::get('/admin/profile', 'Profile')->name('admin.profile');
+    Route::get('/edit/profile', 'EditProfile')->name('edit.profile');
+    Route::post('/store/profile', 'StoreProfile')->name('store.profile');
+
+    Route::get('/change/password', 'ChangePassword')->name('change.password');
+    Route::post('/update/password', 'UpdatePassword')->name('update.password');
+     
 });
+
+
+ // Admin All Route 
+Route::controller(SupplierController::class)->group(function () {
+    Route::get('/supplier/all', 'SupplierAll')->name('supplier.all');
+   ;
+     
+});
+
+ 
+
+
+
+
 
 Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
-
-Route::middleware('auth')->group(function () {
-    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-});
+    return view('admin.index');
+})->middleware(['auth'])->name('dashboard');
 
 require __DIR__.'/auth.php';
+
+
+// Route::get('/contact', function () {
+//     return view('contact');
+// });
